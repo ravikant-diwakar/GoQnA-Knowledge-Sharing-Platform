15427f6f35ee3ed58498c09078ca1a1958de9bf6
ravikant-diwakar <diwakarr9562gmail.com>
2025-05-12 19:10:18 +0530

    12th may

--- src/pages/auth/Signup.tsx
+++ src/pages/auth/Signup.tsx
@@ -326,7 +326,6 @@ const Signup: React.FC = () => {
                 value={username}
                 onChange={(e) => setUsername(e.target.value.toLowerCase())}
                 className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
-                disabled  // Disable to prevent the user from changing it
                 placeholder="Username"
               />
             </div>
